dfc20fb3bd28a080fd95c84c34d1c59be74b5418
Adriana <150440061+Adrianaa911@users.noreply.github.com>
2024-09-11 16:19:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Forecast today
+// const kelvin = 293; initiialy
+const kelvin = 0;
+
+// Convert kelvin in celsius
+const celsius = kelvin - 273;
+
+// Equation to calculate Fahrenheit
+let fahrenheit = celsius * (9 / 5) + 32;
+
+// Method to build -in Math object to round down the fahrenheit temperature
+fahrenheit = Math.floor(fahrenheit);
+
+
+// Use console.log and string interpolation to log the temperature in fahrenheit to the console as follows:
+//The temperature is TEMPERATURE degrees Fahrenheit.
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+
+
+
+// Challenge: Convert Celsius to Newton scale
+// Convert Celsius to Newton scale
+let newton = celsius * (33 / 100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
